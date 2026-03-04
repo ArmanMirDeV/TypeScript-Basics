6d499b8b9e2fd1a93bcb5e224d9955416c358ad5
Arman Mir <mirarman8583@gmail.com>
2026-03-04 13:11:32 +0600

    function to add two numbers

--- firstProgram/index.js
+++ firstProgram/index.js
@@ -2,3 +2,9 @@ var userName = "Arman Mir";
 var age = 25;
 console.log(userName);
 console.log(age);
+// ---------------------------
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+addNumbers(20, 30);
+// addNumbers(20, "30")
--- firstProgram/index.ts
+++ firstProgram/index.ts
@@ -3,3 +3,14 @@ let age = 25;
 
 console.log(userName);
 console.log(age);
+
+
+
+// ---------------------------
+
+
+function addNumbers(num1: number, num2: number) {
+    return num1+num2
+}
+addNumbers(20, 30)
+// addNumbers(20, "30")

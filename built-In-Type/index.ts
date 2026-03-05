1040317e451c1e6491dd1e9dad08681882ceee34
Arman Mir <mirarman8583@gmail.com>
2026-03-05 15:57:32 +0600

    understanding built in data types

---
+++ built-In-Type/index.ts
@@ -0,0 +1,24 @@
+//  Built In data types:  number, string, boolean, void, null, undefined
+
+let userId: number;
+let firstName: string;
+let lastName: string;
+let fullName: string;
+let isActivated: boolean;
+
+userId = 101;
+firstName = "Al Saidul Arman";
+lastName = " Mir";
+isActivated = true;
+
+fullName = firstName.concat(lastName);
+
+
+
+
+console.log(`Your id: ${userId}, username: ${fullName}, account activated: ${isActivated}`);
+
+
+console.log(fullName.split(" "));
+console.log(fullName.toUpperCase());
+console.log(fullName.toLowerCase());

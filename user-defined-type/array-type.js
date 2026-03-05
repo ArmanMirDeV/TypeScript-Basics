799b5391e6b21fc1863c50b22d2386a013c418e6
Arman Mir <mirarman8583@gmail.com>
2026-03-05 17:48:58 +0600

    Array Data Type

---
+++ user-defined-type/array-type.js
@@ -0,0 +1,8 @@
+// let userNames: string[];
+var userNames;
+userNames = ["Arman", "Tetul", "Mir", "Saidul", "Al"];
+console.log(userNames[0]);
+var multipleTypes;
+multipleTypes = [1, "Arman"];
+userNames.sort();
+console.log(userNames);
---
+++ user-defined-type/array-type.ts
@@ -0,0 +1,14 @@
+// let userNames: string[];
+let userNames: Array<string>;
+
+userNames = ["Arman","Tetul", "Mir", "Saidul", "Al"];
+
+console.log(userNames[0]);
+
+let multipleTypes: (string | number)[];
+multipleTypes = [1, "Arman"];
+
+
+
+userNames.sort();
+console.log(userNames);

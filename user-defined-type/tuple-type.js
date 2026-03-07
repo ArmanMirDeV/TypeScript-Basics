3a02981c61a9519778eedb2c1dcd57064ef945e2
Arman Mir <mirarman8583@gmail.com>
2026-03-07 11:04:54 +0600

    Tuple Data Type

---
+++ user-defined-type/tuple-type.js
@@ -0,0 +1,5 @@
+// Mixed data - key, value pairs
+var user;
+user = [105, "Arman Mir"];
+user.push(1066, "Satul");
+console.log(user[0]);
---
+++ user-defined-type/tuple-type.ts
@@ -0,0 +1,11 @@
+// Mixed data - key, value pairs
+
+let user: [number, string];
+user = [105, "Arman Mir"];
+
+
+
+user.push(1066, "Satul")
+
+
+console.log(user[0]);

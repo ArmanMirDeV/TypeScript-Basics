0253966687fb78139c14a8c5de67d03c90cc8afa
Arman Mir <mirarman8583@gmail.com>
2026-03-07 11:40:00 +0600

    any Data Type

---
+++ user-defined-type/any-type.js
@@ -0,0 +1,6 @@
+// let userName;
+var userName;
+userName = "Arman Mir ";
+userName = 158005;
+userName = [14510, "Arman Mir"];
+userName = true;
---
+++ user-defined-type/any-type.ts
@@ -0,0 +1,7 @@
+// let userName;
+let userName: any;
+
+userName = "Arman Mir ";
+userName = 158005;
+userName = [14510, "Arman Mir"];
+userName = true;
